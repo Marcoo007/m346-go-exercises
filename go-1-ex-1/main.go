394fd65feefd0c5317458f5f127c4578789fa4a6
Marco Wismer <Marco.wismer@outlook.om>
2024-11-18 09:00:46 +0100

    changed zodiac to uni code

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -11,7 +11,7 @@ func main() {
 	yearOfBirth := 2007
 	numberOfSiblings := 1
 	heightInMeters := 170
-	zodiacSign := "leo"
+	zodiacSign := "U+264C"
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
