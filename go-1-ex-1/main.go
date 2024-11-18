0c0ca8b0c24420905dfe0ac600512417282779e6
Marco Wismer <Marco.wismer@outlook.om>
2024-11-18 08:47:29 +0100

    startet with the first exercise

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Marco"
+	lastName := "Wismer"
+	dayOfBirth := 27
+	monthOfBirth := 7
+	yearOfBirth := 2007
+	numberOfSiblings := 1
+	heightInMeters := 170
+	zodiacSign := "leo"
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
