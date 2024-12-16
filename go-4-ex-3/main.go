90635f0ad755a94db49ae1b1755538e34f179933
Marco Wismer <Marco.wismer@outlook.om>
2024-12-16 09:31:24 +0100

    finish commit

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,28 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import (
+	"fmt"
+	"math"
+)
+
+// computeQuadraticFormula löst die quadratische Gleichung ax² + bx + c = 0
+func computeQuadraticFormula(a, b, c float64) []float64 {
+	discriminant := math.Pow(b, 2) - 4*a*c
+	if discriminant > 0 {
+		root1 := (-b + math.Sqrt(discriminant)) / (2 * a)
+		root2 := (-b - math.Sqrt(discriminant)) / (2 * a)
+		return []float64{root1, root2}
+	} else if discriminant == 0 {
+		root := -b / (2 * a)
+		return []float64{root}
+	} else {
+		return []float64{}
+	}
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	// Testfälle
+	fmt.Println(computeQuadraticFormula(3, 4, 1)) // Zwei Lösungen
+	fmt.Println(computeQuadraticFormula(2, 4, 2)) // Eine Lösung
+	fmt.Println(computeQuadraticFormula(3, 4, 2)) // Keine Lösung
 }
